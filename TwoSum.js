2755b38e914553a7ad50efde41a8f6b44ffd524c
Airat <parkourtes@gmail.com>
2020-05-03 00:22:06 +0300

    Two Sum

--- TwoSum.js
+++ TwoSum.js
@@ -1,3 +1,44 @@
 // given an arrat of number and target
-// return the indeces of the nubmers that add up to the target 
+// return the indices of the nubmers that add up to the target 
+
+// pointer method:
+// for every Number in nums
+//   - check every OTHER Number
+//     - if == target, return indices
+
+
+const twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const want = target - num;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] === want) return [i, j];
+    }
+  }
+}
+
+// O(n^2), space - O(1);
+
+// hash table 
+// for every Number in nums:
+//   - calculate 'counterpart'
+//   - in table? return indices
+//   - not in table? store w/ index
+
+const newTwoSum = function (sum, target) {
+  const hashTable = {};
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const want = target - num;
+    if (want in hashTable) {
+      const leftIndex = hashTable[want];
+      return [leftIndex, i];
+    } else {
+      hashTable[num] = i;
+    }
+  }
+}
+
+// Time complexity - O(n), space complexity - O(n);
+
 
